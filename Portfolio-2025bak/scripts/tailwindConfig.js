3c58ed235c0f64155dd49d5d9b31797972caa5f2
chrishih <chrishih1982@gmail.com>
2025-08-12 12:06:08 +0800

    Move new portfolio
    移到線上備份，直接在Git上面編輯

---
+++ Portfolio-2025bak/scripts/tailwindConfig.js
@@ -0,0 +1,51 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0f0f0f',
+        secondary: '#1a1a1a'
+      },
+      borderRadius: {
+        'none': '0px',
+        'sm': '4px',
+        DEFAULT: '8px',
+        'md': '12px',
+        'lg': '16px',
+        'xl': '20px',
+        '2xl': '24px',
+        '3xl': '32px',
+        'full': '9999px',
+        'button': '0px'
+      },
+      fontSize: {
+        // 'xs': '0.75rem',
+        // 'sm': '0.875rem',
+        // 'base': '1rem',
+        // 'lg': '1.125rem',
+        // 'xl': '1.25rem',
+        // '2xl': '1.5rem',
+        // '3xl': '1.875rem',
+        // '4xl': '2.25rem',
+        // '5xl': '3rem',
+        // '6xl': '3.75rem',
+        // '7xl': '4.5rem',
+        // '8xl': '6rem',
+        // '9xl': '8rem',
+        'xs': ['0.875rem', '1.5rem'],
+        'sm': ['1rem', '1.75rem'],
+        base: ['1.125rem', '1.75rem'],
+        'lg': ['1.25rem', '2rem'],
+        'xl': ['1.5rem', '2.25rem'],
+        '2xl': ['1.875rem', '2.5rem'],
+        '3xl': ['2.25rem', '1'],
+        '4xl': ['3rem', '1'],
+        '5xl': ['3.75rem', '1'],
+        '6xl': ['4.5rem', '1'],
+        '7xl': ['6rem', '1'],
+        '8xl': ['8re', '1'],
+        '9xl': ['10rem', '1'],
+      }
+    }
+  }
+};
+
